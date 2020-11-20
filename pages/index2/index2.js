e8bdafeccde866d362031d9be41a8eaf33003efb
=bennywork <=1006326944@qq.com>
2020-11-20 18:33:16 +0800

    逻辑标签

--- pages/index2/index2.js
+++ pages/index2/index2.js
@@ -9,8 +9,58 @@ Page({
     staffB: { firstName: 'Shang', lastName: 'You' },
     staffC: { firstName: 'Gideon', lastName: 'Lin' },
     view: 'APP',
-    array: [1, 2, 3, 4, 5]
-  
+    array: [1, 2, 3, 4, 5],
+    id: 0,
+    flag: true,
+    checkflag: true,
+    object: { key: 'hello' },
+    arrays: ['MAIN'],
+    obj1: {
+      a: 1,
+      b: 2
+    },
+    obj2: {
+      c: 3,
+      d: 4
+    },
+    arrays2: [{ message: 'foo' }, { message: 'bar' }],
+    objectArray: [
+      { id: 5, unique: 'unique_5' },
+      { id: 4, unique: 'unique_4' },
+      { id: 3, unique: 'unique_3' },
+      { id: 2, unique: 'unique_2' },
+      { id: 1, unique: 'unique_1' },
+    ],
+    numberArray: [1, 2, 3, 4]
+  },
+
+  switch: function (e) {
+    const length = this.data.objectArray.length;
+    for (let i = 0; i < length; ++i) {
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random() * length)
+      const temp = this.data.objectArray[x]
+      this.data.objectArray[x] = this.data.objectArray[y]
+      this.data.objectArray[y] = temp
+    }
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+
+  addToFront: function (e) {
+    const length = this.data.objectArray.length
+    this.data.objectArray = [{ id: length + 1, unique: 'unique_' + length + 1 }].concat(this.data.objectArray)
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+
+  addNumberToFront: function (e) {
+    this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray)
+    this.setData({
+      numberArray: this.data.numberArray
+    })
   },
 
   /**
